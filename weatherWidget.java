cc0f376cb560a750e077cb13c2daa7b4a56041df
Rohil Arya <rohil.arya@uoit.net>
2019-12-04 10:04:45 -0500

    All tokens initialized

--- weatherWidget.java
+++ weatherWidget.java
@@ -1,12 +1,21 @@
 import java.util.Scanner;
 public class weatherWidget {
   public static void main(String[] args) {
+    //variables
     String cityName;
     int numberOfArgs = 0;
     char comma = ',';
+    String token = "";
+    String cityToken = "";
+    String countryToken = "";
+    String stateToken = "";
+    
+    //getting user input
     Scanner city = new Scanner(System.in);
     System.out.println("Enter your city and country (City,Country or City, Province/State, Country)");
     cityName = city.nextLine();
+    
+    //checking for valid number of arguements
     while (numberOfArgs >= 0)
     {
       for(int i = 0; i < cityName.length(); i++)
@@ -26,9 +35,57 @@ public class weatherWidget {
         System.err.println("Not correct format, use: City,Country or City, Province/State, Country \n Try again.");
         cityName = city.nextLine();
       }
+      //if the number or args are 1 or 2 break out of loop
+      //if the number of args is 2 (city, country)
+      //if the number of args is 3 (city, state, country)
       else
       {
         System.out.println("Correct Format");
+        if(numberOfArgs == 1)
+        {
+          for(int i = 0; i < cityName.length(); i ++)
+          {
+            if(cityName.charAt(i) == comma)
+            {
+              cityToken = token;
+              token = "";
+            }
+            else
+            {
+              token += cityName.charAt(i);
+            }
+          }
+          countryToken = token;
+        }
+        else
+        {
+          int commaCounter = 0;
+          for(int i = 0; i < cityName.length(); i ++)
+          {
+            if(cityName.charAt(i) == comma)
+            {
+              if(commaCounter == 0)
+              {
+                cityToken = token;
+                token = "";
+                commaCounter ++;
+              }
+              else if(commaCounter == 1)
+              {
+                stateToken = token;
+                token = "";
+                commaCounter ++;
+              }
+              
+            }
+            else
+            {
+              token += cityName.charAt(i);
+            }
+          }
+          countryToken = token;
+        }
+        System.out.println("City token is : " + cityToken + " Country token is: " + countryToken + " State/Province token is :" + stateToken);
         break;
       }
     }
